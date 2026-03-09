6a92787d8b3e666070f4ffa66884794b8afb0285
ak4810-creator <ak4810@srmist.edu.in>
2026-03-09 21:55:55 +0530

    Add files via upload

---
+++ OOPSBannerApp (1).java
@@ -0,0 +1,68 @@
+/**
+ * OOPSBannerApp UC5 – Render OOPS as Banner using Inline Array Initialization
+ *
+ * @author Nallana Sai Suraj
+ * @version 6.0
+ */
+public class OOPSBannerApp {
+
+   
+    public static String[] getOPattern() {
+        return new String[]{
+                "   ***   ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "   ***   "
+        };
+    }
+
+        public static String[] getPPattern() {
+        return new String[]{
+                "*******  ",
+                "**    ** ",
+                "**     **",
+                "**    ** ",
+                "*******  ",
+                "**       ",
+                "**       ",
+                "**       ",
+                "**       "
+        };
+    }
+
+    
+    public static String[] getSPattern() {
+        return new String[]{
+                "  *****  ",
+                " **      ",
+                "**       ",
+                " **      ",
+                "  *****  ",
+                "      ** ",
+                "       ** ",
+                "      ** ",
+                " *****  "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(
+                    o[i] + "   " +
+                    o[i] + "   " +
+                    p[i] + "   " +
+                    s[i]
+            );
+        }
+    }
+}
